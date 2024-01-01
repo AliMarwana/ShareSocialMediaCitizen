714c19199e1ddd7983fef3c39a247deeae2ddb9c
AliMarwana <64220279+AliMarwana@users.noreply.github.com>
2024-01-01 11:44:34 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareSocialMedia_2.Models;
+using System.Diagnostics;
+
+namespace ShareSocialMedia_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Sharer()
+        {
+
+            return View("../Views/Home/SharerPage");
+        }
+        [HttpGet]
+        public IActionResult BtnFacebook()
+        {
+
+            string UrlFacebook = Url.Action("Sharer", "Home", null,Request.Scheme);
+            string facebookSharerLink = $"https://www.facebook.com/sharer/sharer.php?u={UrlFacebook}";
+
+            return Json(facebookSharerLink);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function shareOnFacebook() {
+       $.ajax({
+        type: "GET",
+        url: '/Home/BtnFacebook',
+        method: "GET",
+
+        success: function (url) {
+
+            var a = document.createElement("a");
+            a.href = "https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(url);
+            a.target="_blank";
+            a.rel = "noopener noreferrer";
+            a.click();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Custom error : " + jqXHR.responseText + " Status: " + textStatus + " Http error:" + errorThrown);
+        }
+    });
+
+
+}
